092a186bfec9e5e549585effec385b1faf17dc3a
Gerson Lopez <gersonjlv@gmail.com>
2025-06-10 11:32:40 -0600

    eliminacion de panel duplicado
    Se eliminó el panel duplicado

--- client/src/App.vue
+++ client/src/App.vue
@@ -39,9 +39,7 @@
       @show-contact="goTo('contact')"
       @show-parts="goTo('parts')"
       :categoria-inicial="categoriaInicialPiezas"
-    />
-    <AdminLayout v-if="usuario && usuario.rol === 'admin' && currentPage === 'admin'" @navigate="handleSidebarNav" />
-    <AppHeader
+    />    <AppHeader
       v-if="!showLogin"
       :current="currentPage"
       :usuario="usuario"
@@ -99,6 +97,7 @@
       @pago-completado="pagoCompletado"
       @cancelar="cancelarPago"
     />
+    <router-view />
   </div>
 </template>
 
@@ -110,7 +109,6 @@ import Contact from './components/cliente/Contact.vue'
 import CatalogoPiezas from './components/cliente/Piezas.vue'
 import FooterAutoParts from './components/cliente/FooterAutoParts.vue'
 import ScrollToTop from './components/cliente/ScrollToTop.vue'
-import AdminLayout from './components/admin/AdminLayout.vue'
 import CarritoFloatBtn from './components/cliente/Carrito.vue'
 import CarritoModal from './components/cliente/CarritoModal.vue'
 import Pagar from './components/cliente/Pagar.vue'
@@ -129,7 +127,6 @@ export default {
     CatalogoPiezas,
     FooterAutoParts,
     ScrollToTop,
-    AdminLayout,
     CarritoFloatBtn,
     CarritoModal,
     Pagar,
